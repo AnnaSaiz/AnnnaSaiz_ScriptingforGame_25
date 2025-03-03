2e21af5ab05cd5b02241885631731f263313962b
AnnaSaiz <annasaiz@lcad.edu>
2025-03-03 15:09:15 -0800

    Update Key.cs

--- Assets/Scripts/week6/Key.cs
+++ Assets/Scripts/week6/Key.cs
@@ -11,6 +11,7 @@ public class Key : MonoBehaviour
 {
     public KeyColor color;
 
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
